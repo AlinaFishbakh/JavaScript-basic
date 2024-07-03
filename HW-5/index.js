a045c7994b56efcd62ed8efd6cc1586cc12f4b97
Alina Fishbakh <tatsiy.alina@gmail.com>
2024-07-03 14:26:53 +0300

    made arrow function

--- HW-5/index.js
+++ HW-5/index.js
@@ -1,8 +1,5 @@
 //task1//
-function reverseString(str) {
-  return str.split("").reverse().join("")
-}
-
+const reverseString = (str) => str.split("").reverse().join("")
 console.log(reverseString("Hello world!"))
 
 
